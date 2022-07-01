3dc2a77fafe68349844e3e1e6146b36a83639726
Mohammad <heidary100@gmail.com>
2022-07-02 04:24:35 +0430

    Added multiple charts to currency page

--- ClientApp/src/components/Currency.js
+++ ClientApp/src/components/Currency.js
@@ -1,26 +1,107 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import LineAndScatterChart from './LineAndScatterChart';
-import { getCompareData, getLocalData } from "./utils"
-
+import CandleStickChartWithZoomPan from './CandleStickChartWithZoomPan';
+import { getCompareData, getLocalData, getStockData } from "./utils"
+import { Container, Form, Input, Accordion, AccordionItem, AccordionBody, AccordionHeader } from 'reactstrap';
 import { TypeChooser } from "react-stockcharts/lib/helper";
+import LineAndScatterChartGrid from './charts/LineAndScatterChartGrid';
+import LineAndScatterChart from './charts/LineAndScatterChart';
 
 export class Currency extends Component {
     static displayName = Currency.name;
 
     componentDidMount() {
-        getLocalData().then(data => {
-            this.setState({ data })
+        this.setState({ data: [], stock: '', chart: 'line' })
+        const stock = 'GOOG'
+        getStockData(stock).then(data => {
+            console.log('GOt data', data)
+            this.setState({ data, stock })
         })
     }
+
+    setData(data) {
+        this.setState({ data })
+    }
+
+    handleChange(event) {
+        console.log("In select: ", event.target.value);
+        const stock = event.target.value;
+        getStockData(stock).then(data => {
+            console.log('GOt data', data)
+            this.setState({ data, stock })
+        })
+    }
+
+    handleChartChange(event) {
+        const chart = event.target.value;
+        this.setState({ chart })
+
+    }
+
+    renderChart(param) {
+        switch (param) {
+            case 'candle':
+                return <CandleStickChartWithZoomPan type={"hybrid"} data={this.state.data} />;
+            case 'line':
+                return <LineAndScatterChart type={"hybrid"} data={this.state.data} />;
+            case 'line-grid':
+                return <LineAndScatterChartGrid type={"hybrid"} data={this.state.data} />;
+            default:
+                return 'foo';
+        }
+    }
+
     render() {
         if (this.state == null) {
             return <div>Loading...</div>
         }
         return (
-            <TypeChooser>
-                {type => <LineAndScatterChart type={type} data={this.state.data} />}
-            </TypeChooser>
+            <Container style={{ marginTop: 100 }}>
+                <form>
+                    <h2>
+                        Stock:
+                    </h2>
+                    <select className="form-select" aria-label="Select Stock" onChange={this.handleChange.bind(this)}>
+                        <option selected value="GOOG">
+                            Google
+                        </option>
+                        <option value="TSLA">
+                            Tesla
+                        </option>
+                        <option value="MSFT">
+                            Microsoft
+                        </option>
+                        <option value="AAPL">
+                            Apple
+                        </option>
+                        <option value="GME">
+                            GameStop
+                        </option>
+                    </select>
+                    <h2>
+                        Chart type:
+                    </h2>
+                    <select className="form-select" aria-label="Select Chart Type" onChange={this.handleChartChange.bind(this)}>
+                        <option selected value="line">Line Chart</option>
+                        <option value="line-grid">Line Chart with Grid</option>
+                        <option value="candle">Candle Stick Chart</option>
+                    </select>
+                </form>
+
+
+                {this.state.data.length == 0 || this.state.stock == '' || this.state.stock == '' ? (
+                    <Container>
+                        <h3>
+                            Please select a Stock and a Chart Type
+                        </h3>
+                    </Container>
+                ) : (
+                    <Container>
+                        {this.renderChart(this.state.chart)}
+                    </Container>
+                )}
+            </Container>
+
         )
     }
 }
